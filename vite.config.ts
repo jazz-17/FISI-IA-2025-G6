9e8c7629ab9181b2c10db652fe940469d9ce8850
jazz117 <kevinquispe117@gmail.com>
2025-03-31 10:54:14 -0500

    add typescript for shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   base: "https://jazz-17.github.io/FISI-IA-2025-G6/",
   root: "src",
   publicDir: "../public",
@@ -11,5 +18,4 @@ export default defineConfig({
     outDir: '../dist',
     emptyOutDir: true,
   },
-
 })
