33ba3a42c7e108b938c866519ae342662523f766
jazz117 <kevinquispe117@gmail.com>
2025-03-31 10:29:56 -0500

    update base path in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://jazz-17.github.io/FISI-IA-2025-G6/",
   root: "src",
   publicDir: "../public",
   build: {
