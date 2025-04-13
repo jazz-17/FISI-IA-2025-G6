b2e0ce6c57fcfce72199cca5a56c256f8863d0cf
jazz117 <kevinquispe117@gmail.com>
2025-04-13 18:59:44 -0500

    prepare for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,8 +11,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
-
+  base: "https://jazz-17.github.io/FISI-IA-2025-G6/",
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   }
 });
