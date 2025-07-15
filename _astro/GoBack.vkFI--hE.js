09f746cda6a1c91e13ec2b25007cf68980a400e0
kevin <kevinquispe117@gmail.com>
2025-07-15 18:19:17 -0500

    enhance ui

---
+++ _astro/GoBack.vkFI--hE.js
@@ -0,0 +1,6 @@
+import{c as n,_ as s,B as c}from"./index.DJ8Ny2Bb.js";import{d as i,k as _,i as f,f as l,b as p,j as d}from"./runtime-dom.esm-bundler.Ct9KGfPh.js";/**
+ * @license lucide-vue-next v0.488.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const u=n("arrow-left",[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]]),k=i({__name:"GoBack",setup(r,{expose:t}){t();const e={goBack:()=>{const o="/FISI-IA-2025-G6/";window.location.href=o},get ArrowLeft(){return u},Button:c};return Object.defineProperty(e,"__isScriptSetup",{enumerable:!1,value:!0}),e}});function m(r,t,a,e,o,B){return f(),_(e.Button,{variant:"outline",size:"sm",onClick:e.goBack,class:"flex items-center gap-2"},{default:l(()=>[p(e.ArrowLeft,{class:"w-4 h-4"}),t[0]||(t[0]=d(" Volver al Curso "))]),_:1})}const g=s(k,[["render",m]]);export{g as G};
